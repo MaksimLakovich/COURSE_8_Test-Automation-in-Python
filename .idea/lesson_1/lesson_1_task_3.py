e87bd259e3aaebf5bb64d0c59606d23526947f6c
Maksim Lakovich <maks.lakovich@gmail.com>
2024-04-22 17:52:42 +0300

    add my homework #1 for course 8

---
+++ .idea/lesson_1/lesson_1_task_3.py
@@ -0,0 +1,6 @@
+user_name = input("Ваше имя? ")
+print("Привет, " + user_name)
+
+# print("Name? ")
+# x = input()
+# print("Hi, " + x)
---
+++ .idea/lesson_1/lesson_1_task_4.py
@@ -0,0 +1,9 @@
+first_name = input("Ваше имя? ")
+last_name = input("Ваша фамилия? ")
+print("Вас зовут: " + last_name, first_name)
+
+# print("Your first name? ")
+# first_name = input()
+# print("Your last name?")
+# last_name = input()
+# print("Вас зовут: " + last_name, first_name)
